92703d47de42d3e33349b8da645ff476c9051fa2
liuyaming <sarming@126.com>
2020-07-06 22:12:05 +0800

    原生mybatis 使用 @Options注解和@SelectKey注解返回自动生成的主键

--- mybatis/src/main/java/com/atom/mybatis/mapper/BindPhoneMapper.java
+++ mybatis/src/main/java/com/atom/mybatis/mapper/BindPhoneMapper.java
@@ -2,7 +2,6 @@ package com.atom.mybatis.mapper;
 
 import com.atom.mybatis.bean.BindPhoneDO;
 import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.type.JdbcType;
 
 /**
  * 绑定手机信息表
@@ -11,38 +10,6 @@ import org.apache.ibatis.type.JdbcType;
  */
 @Mapper
 public interface BindPhoneMapper {
-
-
-    /**
-     * 根据amwayId查询用户手机绑定信息
-     *
-     * @param amwayId
-     * @return
-     */
-    @Results(id = "baseMap", value = {
-            @Result(column = "id", property = "id", jdbcType = JdbcType.INTEGER, id = true),
-            @Result(column = "creator", property = "creator", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "create_time", property = "createTime", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "modifier", property = "modifier", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "update_time", property = "updateTime", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "version", property = "version", jdbcType = JdbcType.INTEGER),
-            @Result(column = "status", property = "status", jdbcType = JdbcType.INTEGER),
-            @Result(column = "account_id", property = "accountId", jdbcType = JdbcType.BIGINT),
-            @Result(column = "ada", property = "ada", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "amway_id", property = "amwayId", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "mobile_phone", property = "mobilePhone"),
-            @Result(column = "country_code", property = "countryCode", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "master_flag", property = "masterFlag", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bind_date", property = "bindDate", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "channel", property = "channel", jdbcType = JdbcType.VARCHAR)
-    })
-    @Select({
-            "select id,creator,create_time,modifier,update_time,version,status,account_id,ada,amway_id,mobile_phone,country_code,master_flag,bind_date,channel",
-            " from ua_bind_phone t",
-            "where t.amway_id = #{amwayId} and t.status = 1 ",
-            "limit 1 "
-    })
-    BindPhoneDO findByAmwayId(String amwayId);
 
 
     /**
